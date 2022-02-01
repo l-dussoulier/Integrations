4e03cf5af5de6bfa0cf86722fd23cb357323e5d1
Louis <dussoulier.louis@hotmai.fr>
2022-02-01 08:42:41 +0100

    Ajout folders

--- test/CalculsTest.java
+++ test/CalculsTest.java
@@ -1,9 +1,10 @@
-
+package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import src.Calculs;
 
 /**
  * 
--- test/CalculsTest2.java
+++ test/CalculsTest2.java
@@ -1,3 +1,5 @@
+package test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -9,6 +11,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import src.Calculs;
 
 /**
  * @author Yo
